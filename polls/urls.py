22d1d336bb2772b5385bd299de0f80c76df906aa
Tobiasz Kazanowski <tobiaszkazanowski@gmail.com>
2018-04-13 11:27:40 +0200

    first commit, git init

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from polls.views import *
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', vote, name='vote'),
+]
